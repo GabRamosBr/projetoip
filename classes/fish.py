1026187b4fd250f33e463333be7e4501ba00e98a
NetoAlves37 <jessecalves352@gmail.com>
2026-06-17 11:27:24 -0300

    Comentando melhor a classe Fish

--- classes/fish.py
+++ classes/fish.py
@@ -7,38 +7,38 @@ class Fish:
 
     def __init__(self, tela):
 
-        self.fish_types = []
-        self.screen = tela 
-        self.fish_spawn_rate = 1
-        self.fish_list = []
-        self.fish_spawn_counter = 0
-        self.fish_speed = 200
+        self.fish_types = []                #lista com os tipos de peixes
+        self.screen = tela                  #tela onde os peixes serão desenhados
+        self.fish_spawn_rate = 2            #tempo em segundos para o próximo peixe aparecer
+        self.fish_list = []                 #lista com os peixes que estão na tela
+        self.fish_spawn_counter = 0         #temporizador para controlar o spawn dos peixes
+        self.fish_speed = 200               #velocidade dos peixes (em pixels por segundo)
 
 
 #    *TEMPORIZADOR DO SPAWN DE PEIXES* ---------------------------------------------------------
 
     def spawn_time_counter(self, dt):
-        self.fish_spawn_counter += dt
+        self.fish_spawn_counter += dt       #função que inicializa o temporizador
         
 
 #    *GERADOR DE PEIXES* -----------------------------------------------------------------------
 
     def spawn(self, dt):
 
-        if self.fish_spawn_counter >= self.fish_spawn_rate:
+        if self.fish_spawn_counter >= self.fish_spawn_rate:        # se o temporizador for maior ou igual ao tempo de spawn
+                                                             
+            fish = pygame.Rect(1280, random.randint(100, 500), 50, 10)       # um novo peixe é gerado
 
-            fish = pygame.Rect(1280, random.randint(100, 500), 50, 10)
+            self.fish_list.append(fish)          # o peixe é adicionado à lista de peixes na tela
 
-            self.fish_list.append(fish)
+            self.fish_spawn_counter = 0        # e o temporizador é reiniciado para gerar o próximo peixe
 
-            self.fish_spawn_counter = 0
+        for fish in self.fish_list:        # para cada peixe na lista de peixes
 
-        for fish in self.fish_list:
+            fish.x -= self.fish_speed * dt     # a posição X do peixe é decrementada para ele andar para a esquerda
+ 
+            pygame.draw.rect(self.screen, 'cyan', (fish.x, fish.y, 50, 10), width=0)   #o peixe é desenhado com a nova posicao
 
-            fish.x -= self.fish_speed * dt
-
-            pygame.draw.rect(self.screen, 'cyan', (fish.x, fish.y, 50, 10), width=0)
-
-            if fish.x < -50:
+            if fish.x < -50:                        # se o peixe estiver fora da tela ele é removido da lista
                 self.fish_list.remove(fish)
 
